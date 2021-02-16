6506a9b27404e43675a440334758957be497f796
Leonid <belyaevleonid2000@mail.ru>
2021-02-16 11:48:21 +0300

    Зум колесиком (криво)

---
+++ js/zoom.js
@@ -0,0 +1,6 @@
+var zoomable = document.getElementById('map');
+var zX = 0.6;
+window.addEventListener('wheel', function (e) {
+    zX += (e.deltaY > 0) ? -0.1 : 0.1;
+    zoomable.style.transform = 'scale(' + zX + ')';
+});
