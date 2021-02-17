ee8bb2bc8f5ef357989fa17b233c5a68c2dd5328
Leonid <belyaevleonid2000@mail.ru>
2021-02-17 13:40:13 +0300

    Сложно и не работает

--- js/zoom.js
+++ js/zoom.js
@@ -1,6 +1,7 @@
 var zoomable = document.getElementById('map');
 var zX = 0.6;
 window.addEventListener('wheel', function (e) {
+    console.log(e);
     zX += (e.deltaY > 0) ? -0.1 : 0.1;
     zoomable.style.transform = 'scale(' + zX + ')';
 });
