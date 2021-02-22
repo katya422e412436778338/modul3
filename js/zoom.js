ff3c153cd8cd7451f4868286ca7c271063e85889
Leonid <belyaevleonid2000@mail.ru>
2021-02-22 15:02:56 +0300

    Уменьшать масштаб можно только до 0.1

--- js/zoom.js
+++ js/zoom.js
@@ -1,7 +1,7 @@
 var zoomable = document.getElementById('map');
 var zX = 0.6;
 window.addEventListener('wheel', function (e) {
-    console.log(e);
     zX += (e.deltaY > 0) ? -0.1 : 0.1;
-    zoomable.style.transform = 'scale(' + zX + ')';
+    if (!(zoomable.style.transform == 'scale(0.1)' && zX <= 0.1))
+        zoomable.style.transform = 'scale(' + zX + ')';
 });
