c00fa0429e649bf5a57741b7bbc038779dc853ba
Leonid <belyaevleonid2000@mail.ru>
2021-03-07 23:36:55 +0300

    Анимация уток, насекомые, линтер JS

--- js/editor.js
+++ js/editor.js
@@ -1,93 +1,93 @@
 
-var currentMenu = null;
+let currentMenu = null;
 
-var state = {
-    'head': null,
-    'leg': null,
-    'body': null
+let state = {
+  'head': null,
+  'leg': null,
+  'body': null,
 };
 
-var oldState = Object.assign({}, state);
+let oldState = Object.assign({}, state);
 
-var emptyState = Object.assign({}, state);
+const emptyState = Object.assign({}, state);
 
 function toggleSubmenu(menu, flag=1) {
-    if (currentMenu) {
-        let temp = currentMenu;
-        currentMenu = null;
-        toggleSubmenu(temp, 0);
-    }
+  if (currentMenu) {
+    const temp = currentMenu;
+    currentMenu = null;
+    toggleSubmenu(temp, 0);
+  }
 
-    let visibility = flag == 1 ? "visible" : "hidden";
+  const visibility = flag == 1 ? 'visible' : 'hidden';
 
-    let type = menu == 'bodies' ? 'body' : menu.slice(0, -1);
-    
-    
-    document.getElementById('snone').style.visibility = menu != 'legs' ? "hidden" : visibility;
-    
-    document.getElementById(`${menu}_selected`).style.visibility = visibility;
-    
-    if (menu == 'heads') {
-        size = 5;
-    } else if (menu == 'bodies') {
-        size = 6;
-    } else {
-        size = 4;
-    }
- 
-    for (let i = 0; i < size; ++i) {
-        document.getElementById(`s${type}${i}`).style.visibility = visibility;
-    }
-    
-    currentMenu = menu;
+  const type = menu == 'bodies' ? 'body' : menu.slice(0, -1);
+
+
+  document.getElementById('snone').style.visibility = menu != 'legs' ? 'hidden' : visibility;
+
+  document.getElementById(`${menu}_selected`).style.visibility = visibility;
+
+  if (menu == 'heads') {
+    size = 5;
+  } else if (menu == 'bodies') {
+    size = 6;
+  } else {
+    size = 4;
+  }
+
+  for (let i = 0; i < size; ++i) {
+    document.getElementById(`s${type}${i}`).style.visibility = visibility;
+  }
+
+  currentMenu = menu;
 }
 
 function changeState(id) {
-    let wanted_state = {}
+  const wantedState = {};
 
-    if (id != 'snone') {
-        
-        let element = id.slice(1); //head0, leg0, ...
-        let type = element.slice(0, -1); //head, leg, ...
+  if (id != 'snone') {
+    const element = id.slice(1); // head0, leg0, ...
+    const type = element.slice(0, -1); // head, leg, ...
 
-        wanted_state[type] = element;
+    wantedState[type] = element;
+  } else {
+    if (currentMenu == 'legs') {
+      wantedState['leg'] = null;
     } else {
-        if (currentMenu == 'legs') {
-            wanted_state['leg'] = null;
-        } else {
-            wanted_state['body'] = null;
-        }
+      wantedState['body'] = null;
     }
-    
-    render(wanted_state);
+  }
+
+  render(wantedState);
 }
 
-function render(wanted_state) {
-    for (let key in wanted_state) {
-        if (state[key]) {
-            document.getElementById(state[key]).style.visibility = "hidden";
-        }
-
-        if (wanted_state[key])
-            document.getElementById(wanted_state[key]).style.visibility = "visible";
-        state[key] = wanted_state[key];
+function render(wantedState) {
+  for (const key in wantedState) {
+    if (state[key]) {
+      document.getElementById(state[key]).style.visibility = 'hidden';
     }
+
+    if (wantedState[key]) {
+      document.getElementById(wantedState[key]).style.visibility = 'visible';
+    }
+    state[key] = wantedState[key];
+  }
 }
 
 function closeLab() {
-    if (currentMenu) {
-        toggleSubmenu(currentMenu, 0);
-    }
-    oldState = Object.assign({}, state);
-    render(emptyState);
-    closeWindow('lab');
+  if (currentMenu) {
+    toggleSubmenu(currentMenu, 0);
+  }
+  oldState = Object.assign({}, state);
+  render(emptyState);
+  closeWindow('lab');
 }
 
 function showLab() {
-    show('lab');
-    if (currentMenu) {
-        toggleSubmenu(currentMenu, 1);
-    }
-    state = oldState;
-    render(state);
+  show('lab');
+  if (currentMenu) {
+    toggleSubmenu(currentMenu, 1);
+  }
+  state = oldState;
+  render(state);
 }
--- js/main.js
+++ js/main.js
@@ -1,155 +1,186 @@
-var hippo_completed = false;
-var kapusta_completed = false;
-var pigs_completed = false;
-var ducks_completed = false;
+let hippoCompleted = false;
+let kapustaCompleted = false;
+let pigsCompleted = false;
+let ducksCompleted = false;
+let foodCompleted = false;
 
-var chill_mouse_clicked = false;
+let chillMouseClicked = false;
+let insectHandlersSet = false;
 
-var kapusta_clicked = 0;
-var pigs_clicked = 0;
-var ducks_clicked = 0;
+let kapustaClicked = 0;
+let pigsClicked = 0;
+let ducksClicked = 0;
+let insectsClicked = 0;
 
-dragElement(document.getElementById('map')); //Перемещение "как по карте". Украдено из интернета.
+dragElement(document.getElementById('map')); // Перемещение "как по карте". Украдено из интернета.
 
 function dragElement(el) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(el.id + "header")) {
-        document.getElementById(el.id + "header").onmousedown = dragMouseDown;
-    } else {
-        el.onmousedown = dragMouseDown;
-    }
+  let pos1 = 0; let pos2 = 0; let pos3 = 0; let pos4 = 0;
+  if (document.getElementById(el.id + 'header')) {
+    document.getElementById(el.id + 'header').onmousedown = dragMouseDown;
+  } else {
+    el.onmousedown = dragMouseDown;
+  }
 
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag; 
-    }
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
 
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        el.style.top = (el.offsetTop - pos2) + "px";
-        el.style.left = (el.offsetLeft - pos1) + "px";
-    }
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    el.style.top = (el.offsetTop - pos2) + 'px';
+    el.style.left = (el.offsetLeft - pos1) + 'px';
+  }
 
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 function closeWindow(e) {
-    document.getElementById(e).style.visibility = "hidden";
+  document.getElementById(e).style.visibility = 'hidden';
 }
 
 function show(e) {
-    document.getElementById(e).style.visibility = "visible";
+  document.getElementById(e).style.visibility = 'visible';
 }
 
-function hover(e, d){
-    document.getElementById(e).style.visibility = d == 1 ? "visible" : "hidden"; 
+function hover(e, d) {
+  document.getElementById(e).style.visibility = d == 1 ? 'visible' : 'hidden';
 }
 
-function hippo() { //Бегемот, показать конфеты и окрасить в другой цвет
-    if (!hippo_completed) {
-        show('windowY');
-        var bucket = document.getElementById('bucket');
-        bucket.classList.add('button');
-        bucket.onclick = ()=>{
-            show('candy');
-            show('color0');
-            show('color1');
-            // closeWindow('color3');
-            closeWindow('windowY');
-            hippo_completed = true;
-            bucket.onclick = null;
-            bucket.classList.remove('button');
-            unclickable('animal12');
-        }
-    }
+function hippo() { // Бегемот, показать конфеты и окрасить в другой цвет
+  if (!hippoCompleted) {
+    show('windowY');
+    const bucket = document.getElementById('bucket');
+    bucket.classList.add('button');
+    bucket.onclick = ()=>{
+      show('candy');
+      show('color0');
+      show('color1');
+      // closeWindow('color3');
+      closeWindow('windowY');
+      hippoCompleted = true;
+      bucket.onclick = null;
+      bucket.classList.remove('button');
+      unclickable('animal12');
+    };
+  }
 }
-
 
 
 function kapusta() {
-    if (!kapusta_completed) {
-        show('window1');
-        for (let i = 1; i <=5; ++i) {
-            let el = document.getElementById(`kap${i}`);
-            el.classList.add('button');
-            el.onclick = ()=>{
-                kapusta_clicked++;
-                closeWindow(`kap${i}`);
-                if (kapusta_clicked == 5) {
-                    kapusta_completed = true;
-                    closeWindow('window1');
-                    unclickable('animal0');
-                }
-            }
+  if (!kapustaCompleted) {
+    show('window1');
+    for (let i = 1; i <=5; ++i) {
+      const el = document.getElementById(`kap${i}`);
+      el.classList.add('button');
+      el.onclick = ()=>{
+        kapustaClicked++;
+        closeWindow(`kap${i}`);
+        if (kapustaClicked == 5) {
+          kapustaCompleted = true;
+          closeWindow('window1');
+          unclickable('animal0');
         }
+      };
     }
+  }
 }
 
 function unclickable(id) {
-    let e = document.getElementById(id);
-    e.onmouseover = null;
-    e.classList.remove('button');
+  const e = document.getElementById(id);
+  e.onmouseover = null;
+  e.classList.remove('button');
 }
 
 function mouse() {
-    if (!chill_mouse_clicked) {
-        show('window8');
-        document.getElementById('cheese').classList.add('vert-move');
-        document.getElementById('rect330').onclick = ()=>{
-            closeWindow('window8');
-            chill_mouse_clicked = true;
-            unclickable('animal4');
-            document.getElementById('cheese').classList.remove('vert-move');
-        }
-    }
+  if (!chillMouseClicked) {
+    show('window8');
+    document.getElementById('cheese').classList.add('vert-move');
+    document.getElementById('rect330').onclick = ()=>{
+      closeWindow('window8');
+      chillMouseClicked = true;
+      unclickable('animal4');
+      document.getElementById('cheese').classList.remove('vert-move');
+    };
+  }
 }
 
 function pigs() {
-    if (!pigs_completed) {
-        show('window2');
-        for (let i = 0; i <=4; ++i) {
-            let el = document.getElementById(`pig${i}`);
-            el.classList.add('button');
-            el.onclick = ()=>{
-                pigs_clicked++;
-                closeWindow(`pig${i}`);
-                if (pigs_clicked == 5) {
-                    pigs_completed = true;
-                    closeWindow('window2');
-                    unclickable('animal1');
-                }
-            }
+  if (!pigsCompleted) {
+    show('window2');
+    for (let i = 0; i <=4; ++i) {
+      const el = document.getElementById(`pig${i}`);
+      el.classList.add('button');
+      el.onclick = ()=>{
+        pigsClicked++;
+        closeWindow(`pig${i}`);
+        if (pigsClicked == 5) {
+          pigsCompleted = true;
+          closeWindow('window2');
+          unclickable('animal1');
         }
+      };
     }
+  }
 }
 
 function duck() {
-    if (!ducks_completed) {
-        show('windowX');
-        for (let i = 0; i < 3; ++i) {
-            let duck = document.getElementById(`duck${i}`);
-            duck.classList.add('button');
-            duck.onclick = () => {
-                duck.classList.add(`duck${i}-home`);
-                ducks_clicked++;
-                if (ducks_clicked == 3) {
-                    ducks_completed = true;
-                    closeWindow('windowX');
-                    unclickable('animal13');
-                }
-            }
+  if (!ducksCompleted) {
+    show('windowX');
+    for (let i = 0; i < 3; ++i) {
+      const duck = document.getElementById(`duck${i}`);
+      duck.classList.add('button');
+      duck.onclick = () => {
+        duck.classList.add(`duck${i}-home`);
+        ducksClicked++;
+        if (ducksClicked == 3) {
+          ducksCompleted = true;
+          closeWindow('windowX');
+          unclickable('animal13');
         }
+      };
     }
+  }
+}
+
+function food() {
+  if (!foodCompleted) {
+    show('window7');
+    for (let i = 0; i < 9; ++i) {
+      const insect = document.getElementById(`insect${i}`);
+      if (!insectHandlersSet) {
+        insect.classList.add('anim', 'button');
+        insect.onclick = () => {
+          console.log(`insect${i} clicked`);
+          insectsClicked++;
+          console.log(insectsClicked);
+          insect.remove();
+          if (insectsClicked == 9) {
+            foodCompleted = true;
+            closeWindow('window7');
+            unclickable('animal3');
+          }
+        };
+      }
+      insect.style.transform = `translate(${rand(-500, 500)}px, ${rand(-500, 500)}px)`;
+    }
+    insectHandlersSet = true;
+  }
+}
+
+function rand(min, max) {
+  return Math.random() * (max - min) + min;
 }
